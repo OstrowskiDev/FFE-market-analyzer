0e578a1043bc06b29fcc1232f6fffd54268f35ea
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-04-17 07:04:56 +0200

    Add: language: eng option to OCR space api for improved output. Also added possibility of using .env for passing secret keys as an alternative to using OCR Space API free public api key "helloworld".

--- pipeline/runOcr.js
+++ pipeline/runOcr.js
@@ -1,13 +1,15 @@
 import { ocrSpace } from "ocr-space-api-wrapper"
 
 export async function runOcr() {
+  const API_KEY = process.env.API_KEY || "helloworld"
   let result
   try {
     // Using the OCR.space default free API key (max 10reqs in 10mins) + remote file
     result = await ocrSpace("debug_threshold.png", {
-      apiKey: "helloworld",
+      apiKey: API_KEY,
       OCREngine: "2",
       isTable: true, // wymusza zwrot tekstu linia po linii - ważne dla tabelki
+      language: "eng",
     })
 
     // Using your personal API key + local file
