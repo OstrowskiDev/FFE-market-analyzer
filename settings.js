8c1c760894bee58cb97f00fc3b1a3a1355fc6a11
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2026-04-27 21:15:55 +0200

    Add: settings file to provide users with easy way to change various aspects of this application.

---
+++ settings.js
@@ -0,0 +1,8 @@
+export const settings = {
+  publicFreeApiKey: "helloworld",
+  commanderName: "Decados", //name will be displayed on welcome screen
+  screenshotDir: "", //absolute path to dir where dosbox-x stores screenshot files; example: "/games/dosbox-x/capture"
+  ffeMarketNavigatorDir: "", //absolute path to dir where FFE Market Navigator main.js file is located; example: "/games/FFE/FFE Market Navigator"
+  noFluff: true, // change to true if you don't want to see CLI visual effects
+  ignoredGoods: ["Alien Artefacts"], // list of goods that will not show as results of trade route analysis
+}
